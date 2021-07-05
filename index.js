56c86cf01b7f34cf8f39a3c6d831514716991a8f
Long Phu <phutl@icd-vn.com>
2021-07-05 11:10:40 +0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+const path = require('path')
+const port = 3000
+
+var appleAppSiteAssociationContents = fs.readFileSync(path.resolve(__dirname, './apple-app-site-association'), 'utf8');
+
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.sendFile(path.resolve(__dirname, './apple-app-site-association'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
