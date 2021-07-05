8aa08c7c614d5326f8ad5a17720c366eaa653edf
Long Phu <phutl@icd-vn.com>
2021-07-05 11:37:09 +0700

    update content type

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000
 var appleAppSiteAssociationContents = fs.readFileSync(path.resolve(__dirname, './apple-app-site-association'), 'utf8');
 
 app.get('/', (req, res) => {
-    res.set('Content-Type', 'application/json');
+    res.set('content-Type', 'application/json');
     res.sendFile(path.resolve(__dirname, './apple-app-site-association'));
 })
 
